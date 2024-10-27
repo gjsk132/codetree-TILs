c2d1b4fc6a08e1d070a802db4c198d2e969eb93d
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-10-27 22:53:42 +0900

    Title: 진법 변환 7, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241027/진법 변환 7/base-conversion-7.py
@@ -0,0 +1,13 @@
+x, y = map(int,input().split("."))
+x = bin(x)+"."
+x = list(x[2:])
+
+length = 10**len(str(y))
+
+for _ in range(4):
+    y *= 2
+    
+    x.append(str(y//length))
+    y %= length
+
+print(*x, sep="")
