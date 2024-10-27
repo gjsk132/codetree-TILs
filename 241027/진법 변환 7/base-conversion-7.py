8613e8e6208ddc3b4e201335b444e28efa29c61f
Heo Nayeong <113815454+gjsk132@users.noreply.github.com>
2024-10-27 22:57:58 +0900

    Title: 진법 변환 7, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241027/진법 변환 7/base-conversion-7.py
+++ 241027/진법 변환 7/base-conversion-7.py
@@ -2,16 +2,15 @@ x, y = map(int,input().split("."))
 x = bin(x)+"."
 x = list(x[2:])
 
-length = 10**len(str(y))
+y = float(y/10**len(str(y)))
 
 for _ in range(4):
-    if not y:
-        x.append("0")
-        continue
-    
     y *= 2
     
-    x.append(str(y//length))
-    y %= length
+    if y >= 1:
+        x.append("1")
+        y-=1
+    else:
+        x.append("0")
 
 print(*x, sep="")
